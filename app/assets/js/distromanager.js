c3cbc8bc30baa59930c946db6778956c4a39c453
Najoan <68422289+najoan125@users.noreply.github.com>
2025-02-28 16:20:57 +0900

    2.1.5

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://ww1.apcl.kro.kr/nfclient/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://hyfata.kr/nfclient/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
